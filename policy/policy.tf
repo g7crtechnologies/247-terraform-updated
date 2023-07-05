117e80764b7b67d6dedec19b8c2cb04ff1de15a7
Deependra singh gurjar <116145386+deependra-singh-gurjar@users.noreply.github.com>
2023-07-05 13:00:48 +0530

    Delete policy directory

--- policy/policy.tf
+++
@@ -1,9 +0,0 @@
-#------------------------------------------
-# Module for Azure Policies
-#------------------------------------------
-module "Azure_policies" {
-  source       = "../module/tfs_azurerm_policy"
-  policy       = var.policy
-  display_name = var.display_name
-  policy_rg    = var.policy_rg
-}
