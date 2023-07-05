001e6f59eec196970d5e3f2b825887336929a420
Deependra singh gurjar <116145386+deependra-singh-gurjar@users.noreply.github.com>
2023-07-05 13:00:58 +0530

    Delete private_dns directory

--- private_dns/module.tf
+++
@@ -1,6 +0,0 @@
-module "dnsresolver" {
-  source                = "../module/tfs_azurerm_private_dns"
-  location              = var.location
-  private_dns_zone_name = var.private_dns_zone_name
-  tags                  = var.tags
-}
--- private_dns/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "domain_name" {
-  value = module.dnsresolver.domain_name
-}
-output "domain_id" {
-  value = module.dnsresolver.domain_id
-}
